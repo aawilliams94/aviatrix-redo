e76e402abe8f6ea5a2e3b28cf7f6dd1ef64d865d
Ashanti Williams <ashanti.a.williams@hotmail.com>
2019-06-10 06:54:30 -0500

    Initial commit

---
+++ AviatrixApp/Aviatrix.swift
@@ -0,0 +1,39 @@
+//
+//  Aviatrix.swift
+//  AviatrixApp
+//
+//  Created by Amy Holt on 6/10/18.
+//  Copyright © 2018 Amy Holt. All rights reserved.
+//
+
+import Foundation
+
+class Aviatrix {
+  
+    var author : String
+    init(authorName : String ) {
+        author = authorName
+    }
+    
+    var running = false
+    func start() -> Bool {
+        running = true
+        return true
+    }
+    
+    func refuel() {
+        
+    }
+    
+    func flyTo(destination : String) {
+        
+    }
+    
+    func distanceTo(target : String) {
+    
+    }
+    
+    func knownDestinations() -> [String] {
+       return ["St. Louis"]
+    }
+}
